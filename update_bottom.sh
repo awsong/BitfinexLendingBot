c8de8165461ebe4ec7c4f61dd5f66a88eb0c27e0
Andy W. Song <wsong.cn@gmail.com>
2021-05-25 08:20:57 +0800

    update_bottom.sh

--- update_bottom.sh
+++ update_bottom.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-d=`gdate -d '1/2 hour ago' '+%s'`
+d=`/bin/date -d '30 minutes ago' '+%s'`
 
-export nr=`grep ,2 bit.txt |grep -v ']]]'|cut -d ',' -f 4,5,6|awk -v var=$d -F'[ =]' '$1 > var' | cut -d ',' -f 3|sort|tail -1`
+export nr=`tail -20000 bit.txt |grep ,2 |grep -v ']]]'|cut -d ',' -f 4,5,6|/usr/bin/awk -v var=$d -F'[ =]' '$1 > var' | cut -d ',' -f 3|sort|tail -1`
+export nr=0`echo $nr*100|/usr/bin/bc`
 
-jq '.[0].strategy.MarginBot.MinDailyLendRate = env.nr' default.conf | sponge default.conf
+#/usr/local/bin/jq '.[0].strategy.MarginBot.MinDailyLendRate = env.nr' default.conf | /usr/local/bin/sponge default.conf
+echo $nr
+low_bound=0.01979
+if [[ $nr < $low_bound ]] 
+then  
+    nr=$low_bound
+fi
+echo $nr
+/bin/sed -i "s/\(\"MinDailyLendRate\":\).*/\1 $nr,/" default.conf
